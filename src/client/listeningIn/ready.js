0cbdfc613afcf34c121d34a38bbfa76dfbeef5f0
gabrielanselmo <gabriel.hizoul@gmail.com>
2021-09-05 05:12:55 -0300

    ready.js

---
+++ src/client/listeningIn/ready.js
@@ -0,0 +1,34 @@
+const Guild = require("../../database/Schemas/Guild"),
+  User = require("../../database/Schemas/User"),
+  Commands = require("../../database/Schemas/Command"),
+  Client = require("../../database/Schemas/Client");
+
+module.exports = class {
+  constructor(client) {
+    this.client = client;
+  }
+
+  async run() {
+    this.client.database.users = User;
+    this.client.database.guilds = Guild;
+    this.client.database.clientUtils = Client;
+    this.client.database.commands = Commands;
+
+
+    const status = [
+      {
+        name: "twitch.tv/ratoborrachudo",
+      },
+      {
+        name: "Desenvolvido pelo Biel",
+      },
+    ];
+    setInterval(() => {
+      var randomStatus = status[Math.floor(Math.random() * status.length)];
+      this.client.user.setActivity(randomStatus.name);
+    }, 10 * 1000);
+
+    this.client.user.setStatus("dnd");
+    this.client.music.init(this.client.user.id)
+  }
+};
